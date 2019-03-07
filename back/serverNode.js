6e6dce6ed096e9fd85af3ad2142ec6647135af24
JeanVernus <jeannywcs@gmail.com>
2019-03-07 17:35:10 +0100

    les vioks sont dans la place

--- back/serverNode.js
+++ back/serverNode.js
@@ -1,14 +1,18 @@
+const http = require('http');
+const path = require('path');
 const mySQL = require('./configMysql');
 const express = require('express');
 const app = express();
 const port = 5000;
-
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 // Support JSON-encoded bodies
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 // Support URL-encoded bodies
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: false
 }));
 
 app.get('/', (request, response) => {
@@ -17,6 +21,7 @@ app.get('/', (request, response) => {
 
 app.listen(port, (err) => {
   if (err) {
+    webkitRTCPeerConnection
     throw new Error('Something bad happened...');
   }
 
@@ -86,11 +91,30 @@ app.listen(port, (err) => {
 //     }
 //   });
 // });
+app.post("/signup", (req, res) => {
+  const lastname = req.body.lastname;
+  const name = req.body.name;
+  const password = req.body.password;
+  const email = req.body.email;
 
+  var sql = 'INSERT INTO les_vioks (firstname, lastname, email, password) VALUES ('+mySQL.escape(lastname)+', '+mySQL.escape(name)+','+mySQL.escape(email)+','+mySQL.escape(password)+')';
+  console.log("test req sql",sql);
+  mySQL.query(sql, (err, results) => {
+    if (err) {
+      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+      console.log(err);
+      res.status(500).send("Oups, Erreur lors de la sauvegarde");
+    } else {
+      // Si tout s'est bien passé, on envoie un statut "ok".
+      res.sendStatus(200);
+    }
+  })
+
+})
 
 app.get('/api/vioks', (req, res) => {
   mySQL.query(`SELECT * from les_vioks`, (err, results) => {
-     if (err) {
+    if (err) {
       console.log(err.sqlMessage);
       res.status(500).send('Erreur lors de la récupération : ' + err.sqlMessage);
     } else {
--- front/src/Component/SignUp.js
+++ front/src/Component/SignUp.js
@@ -1,58 +1,60 @@
 import React, { Component } from 'react';
-import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2';
+// import { Button } from 'semantic-ui-react'
 
 class SignUp extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            mail: "Renseigner votre email",
-            name: "Renseigner votre Prénom",
-            lastName: "Renseigner votre nom",
-            password: "Renseigner votre mot de passe",
-            verifPassword: "Renseigner votre mot de passe",
-        }
+        // this.state = {
+        //     mail: "Renseigner votre email",
+        //     name: "Renseigner votre Prénom",
+        //     lastName: "Renseigner votre nom",
+        //     password: "Renseigner votre mot de passe",
+        //     verifPassword: "Renseigner votre mot de passe",
+        // }
 
         console.log(this.state);
     }
-    updateEmailField = () => {
+    // updateEmailField = () => {
 
 
-            const inputMail= document.getElementById('emailId').value;
-            const inputName = document.getElementById('nameId').value;
-            const inputLastName = document.getElementById('lastnameId').value;
-            const inputPassword = document.getElementById('passwordId').value;
-            const inputVerifPassword = document.getElementById('verifpasswordId').value;
+    //         const inputMail= document.getElementById('emailId').value;
+    //         const inputName = document.getElementById('nameId').value;
+    //         const inputLastName = document.getElementById('lastnameId').value;
+    //         const inputPassword = document.getElementById('passwordId').value;
+    //         const inputVerifPassword = document.getElementById('verifpasswordId').value;
        
 
-        if (inputPassword !== inputVerifPassword) {
+    //     if (inputPassword !== inputVerifPassword) {
             
-            console.log("test comparaison");
+    //         console.log("test comparaison");
             
 
-            Swal.fire({
-                type: "error",
-                title: "Oops...",
-                text: "Mauvais mot de passe"
-            })
-        }
-        else{
-            this.setState({
-                mail: inputMail,
-                name: inputName,
-                lastName: inputLastName,
-                password: inputPassword,
-                verifPassword: inputVerifPassword
-            });
-        }
-    }
+    //         Swal.fire({
+    //             type: "error",
+    //             title: "Oops...",
+    //             text: "Mauvais mot de passe"
+    //         })
+    //     }
+    //     else{
+    //         this.setState({
+    //             mail: inputMail,
+    //             name: inputName,
+    //             lastName: inputLastName,
+    //             password: inputPassword,
+    //             verifPassword: inputVerifPassword
+    //         });
+    //     }
+    // }
     render() {
 
        
 
         console.log("test json", JSON.stringify(this.state));
+
         return (
             <div>
-
+            <form action="http://192.168.16.237:5000/signup" method="post">
                 <h1 className="inputname"> Renseigner votre prénom</h1>
                 <input className="inputlastname" type="text" name="name" id="nameId" />
 
@@ -66,15 +68,16 @@ class SignUp extends Component {
                 <input className="inputPassword" type="text" name="password" id="passwordId" />
 
                 <h1 className="inputverifPassword">verif-Password</h1>
-                <input className="inputverifPassword" type="text" name="verifpasswordId" id ="verifpasswordId" /><br />
+                <input className="inputverifPassword" type="text" name="verifpassword" id ="verifpasswordId" /><br />
 
 
-                <button type="submit" onClick={this.updateEmailField}>Soumettre</button>
-
+                <button type="submit" >Soumettre</button>
+                
 
                 <h1>{JSON.stringify(this.state)}</h1>
                
-
+            </form>
+           
 
             </div>
         )
